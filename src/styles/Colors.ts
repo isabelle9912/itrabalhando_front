aa7a1ab7be5576e507a877eb90d657abbc047961
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 11:12:19 -0300

    feat: creates the application color palette file

---
+++ src/styles/Colors.ts
@@ -0,0 +1,29 @@
+const Colors = {
+    colorBrand0: "#00AEEF",
+    colorBrand1: "#BDBDBD",
+    colorBrand2: "#1E1E1E",
+    colorBrand3: "#5CB85C",
+    colorBrand4: "#007A33",
+
+    grey10: "#FDFDFD",
+    grey9: "#F8F9FA",
+    grey8: "#F1F3F5",
+    grey7: "#E9ECEF",
+    grey6: "#DEE2E6",
+    grey5: "#CED4DA",
+    grey4: "#ADB5BD",
+    grey3: "#868E96",
+    grey2: "#495057",
+    grey1: "#212529",
+    grey0: "#0B0D0D",
+
+    alert1: "#C34E53",
+    alert2: "#FDD8D8",
+    warning: "#FFCD07",
+    success1: "#77C65A",
+    success2: "#CCEBD7",
+    succes3: "#DDF3E4",
+    information: "#155BCB",
+};
+
+export default Colors;
