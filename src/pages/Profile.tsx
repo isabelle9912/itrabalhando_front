c2decef980be0153db75bc1aa28b613fd4380b5b
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 11:06:28 -0300

    feat: creates the Profile page file

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+const Profile: React.FC = () => {
+    return (
+        <Container>
+            <h1>Profile Page</h1>
+        </Container>
+    )
+}
+
+export default Profile;
