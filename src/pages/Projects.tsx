f49421f9854514f9fd6012483cffda1ef6409b85
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 11:06:36 -0300

    feat: creates the Projects page file

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+const Projects: React.FC = () => {
+    return (
+        <Container>
+            <h1>Projects Page</h1>
+        </Container>
+    )
+}
+
+export default Projects;
