d735060d842d743fe0b1ea1fd1fcf93b1beeef5d
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 13:33:54 -0300

    feat: adds state management for proposals

--- src/context/ProjectContext.tsx
+++ src/context/ProjectContext.tsx
@@ -1,16 +1,20 @@
 import { createContext, useContext, useState } from 'react';
 import { Project, ProjectFormData } from '../schemas/projectSchema';
+import { Proposal, ProposalFormData } from '../schemas/proposalSchema';
 import { mockProjects } from '../data/mockProjects';
 
 type ProjectContextType = {
     projects: Project[];
+    proposals: Proposal[];
     addProject: (project: ProjectFormData) => void;
+    addProposal: (proposal: ProposalFormData) => void;
 };
 
 const ProjectContext = createContext<ProjectContextType | null>(null);
 
 export const ProjectProvider = ({ children }: { children: React.ReactNode }) => {
     const [projects, setProjects] = useState<Project[]>(mockProjects);
+    const [proposals, setProposals] = useState<Proposal[]>([]);
 
     const addProject = (project: ProjectFormData) => {
         const newProject: Project = {
@@ -20,8 +24,16 @@ export const ProjectProvider = ({ children }: { children: React.ReactNode }) => 
         setProjects((prev) => [...prev, newProject]);
     };
 
+    const addProposal = (proposal: ProposalFormData) => {
+        const newProposal: Proposal = {
+            ...proposal,
+            id: String(proposals.length + 1), // Gera um ID único
+        };
+        setProposals((prev) => [...prev, newProposal]);
+    };
+
     return (
-        <ProjectContext.Provider value={{ projects, addProject }}>
+        <ProjectContext.Provider value={{ projects,proposals, addProject, addProposal }}>
             {children}
         </ProjectContext.Provider>
     );
