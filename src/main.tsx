ae71b5757ee789d602349c194cad348bae14e468
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 12:43:15 -0300

    feat: add the project provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {UserProvider} from "./context/UserContext.tsx";
+import {ProjectProvider} from "./context/ProjectContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <UserProvider>
-        <App />
+          <ProjectProvider>
+            <App />
+          </ProjectProvider>
       </UserProvider>
   </StrictMode>,
 )
