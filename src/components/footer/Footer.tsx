eb248d3ad5c0c2fd9af6388a50719fbf0152e8d6
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 11:13:35 -0300

    fix: change footer information

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import Colors from "../../styles/Colors.ts";
 
 const Footer: React.FC = () => (
     <footer style={{ backgroundColor: Colors.colorBrand4, color: Colors.grey10, padding: '20px 0', textAlign: 'center' }}>
-        <p>Contato: contato@olimpiadatech.com | (XX) XXXX-XXXX</p>
-        <p>© 2025 Olimpíada de Programação Comunidade Tech. Todos os direitos reservados.</p>
+        <p>Contato: contato@freelancersita.com | (XX) XXXX-XXXX</p>
+        <p>© 2025 Freelancers Itabaiana. Todos os direitos reservados.</p>
     </footer>
 )
 
