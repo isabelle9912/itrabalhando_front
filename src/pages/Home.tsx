4cd40054cd41721bf66e62d6930ace209966f87d
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 11:06:22 -0300

    feat: creates the Home page file

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+const Home: React.FC = () => {
+    return (
+        <Container>
+            <h1>Home Page</h1>
+        </Container>
+    )
+}
+
+export default Home;
