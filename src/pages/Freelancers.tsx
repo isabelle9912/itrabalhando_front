191e7f2c4e9b7c1c8fd4d2e748dba12e00c802f0
saahirah <isabellesaahirah28@gmail.com>
2025-03-06 11:06:17 -0300

    feat: creates the Freelancers page file

---
+++ src/pages/Freelancers.tsx
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+const Freelancers: React.FC = () => {
+    return (
+        <Container>
+            <h1>Freelancers Page</h1>
+        </Container>
+    )
+}
+
+export default Freelancers;
