8bf2eea8487719ce03c448055fe5a91eeac44f9c
saahirah <isabellesaahirah28@gmail.com>
2025-03-07 11:18:57 -0300

    feat: creates the CardFreelancer component

---
+++ src/components/cardFreelancer/CardFreelancer.tsx
@@ -0,0 +1,32 @@
+import {Card} from "react-bootstrap";
+import ProfileImgName from "../profileImgName/ProfileImgName.tsx";
+
+interface Props {
+    id: string;
+    name: string;
+    bio: string;
+    skills: string[];
+    email: string;
+    image: string | undefined;
+}
+
+const CardFrelancer = ({id, name, bio, skills, email, image}: Props) => {
+    return (
+        <Card>
+            <Card.Body>
+                {image && (
+                    <ProfileImgName id={id} name={name} image={image}/>
+                )}
+                <Card.Text>{bio}</Card.Text>
+                <Card.Text>
+                    <strong>Habilidades:</strong> {skills.join(', ')}
+                </Card.Text>
+                <Card.Text>
+                    <strong>E-mail:</strong> {email}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default CardFrelancer;
