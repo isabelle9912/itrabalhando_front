3eccef463284558b8315a0027e65bc70f585eea3
saahirah <isabellesaahirah28@gmail.com>
2025-03-07 11:37:12 -0300

    feat: adds theme provider and a button to switch between dark and light

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,36 @@ import Profile from './pages/profile/Profile.tsx';
 import CustomNavbar from "./components/navbar/Navbar.tsx";
 import ProjectDetails from "./pages/projectDetails/ProjectDetails.tsx";
 import CreateProject from "./pages/createProject/CreateProject.tsx";
+import {ThemeProvider, useTheme} from "./context/ThemeContext.tsx";
+import {LuMoon, LuSun} from "react-icons/lu";
+
+const ThemeToggleButton = () => {
+    const { theme, toggleTheme } = useTheme();
+    return (
+        <button className="btn" onClick={toggleTheme}>
+            {theme === "dark" ? <LuSun /> : <LuMoon />}
+        </button>
+    );
+};
 
 function App() {
     return (
-        <Router>
-            <CustomNavbar/>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/freelancers" element={<Freelancers />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/project/:id" element={<ProjectDetails />} />
-                <Route path="/profile/:id" element={<Profile />} />
-                <Route path="/create-project" element={<CreateProject />} />
-            </Routes>
-        </Router>
+        <ThemeProvider>
+            <Router>
+                <CustomNavbar/>
+                <div className="container mt-3">
+                    <ThemeToggleButton />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/freelancers" element={<Freelancers />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/project/:id" element={<ProjectDetails />} />
+                        <Route path="/profile/:id" element={<Profile />} />
+                        <Route path="/create-project" element={<CreateProject />} />
+                    </Routes>
+                </div>
+            </Router>
+        </ThemeProvider>
     );
 }
 
