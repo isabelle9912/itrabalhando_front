10d8ae01a2def27b1b1fb9c92d8d2ec730c62d72
saahirah <isabellesaahirah28@gmail.com>
2025-03-13 15:44:33 -0300

    feat: adds the context call to search for a user in the api

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -3,14 +3,23 @@ import { useUserContext } from '../../context/UserContext';
 import { Card, Container, Button } from 'react-bootstrap';
 import { isFreelancer, isClient } from '../../utils/typeGuards';
 import ProfileImgName from "../../components/profileImgName/ProfileImgName.tsx";
+import {useEffect, useState} from "react";
+import {iClient} from "../../interfaces/client.interface.ts";
+import {iFreelancer} from "../../interfaces/freelancer.interface.ts";
 
 const Profile = () => {
     const { id } = useParams<{ id: string }>();
-    const { users } = useUserContext();
+    const [user, setUser] = useState<iClient | iFreelancer | null>()
+    const { retriveUser } = useUserContext();
 
-    // Encontra o usuário pelo ID
-    const user = users.find((user) => user.id === id);
+    useEffect(() => {
+        const getUser = async () => {
+            const user = await retriveUser(Number(id));
+            setUser(user);
+        }
 
+        getUser();
+    }, [retriveUser]);
     if (!user) {
         return <Container className="mt-4">Perfil não encontrado.</Container>;
     }
@@ -19,22 +28,22 @@ const Profile = () => {
         <Container className="mt-4">
             <Card>
                 <Card.Body>
-                    <ProfileImgName id={user.id} name={user.data.name} image={user?.data.image} />
-                    {isFreelancer(user.data) && (
+                    <ProfileImgName id={user.id} name={user.name} image={user?.image} />
+                    {isFreelancer(user) && (
                         <>
-                            <Card.Text>{user.data.bio}</Card.Text>
+                            <Card.Text>{user.bio}</Card.Text>
                             <Card.Text>
-                                <strong>Habilidades:</strong> {user.data.skills.join(', ')}
+                                <strong>Habilidades:</strong> {user.skills.join(', ')}
                             </Card.Text>
                         </>
                     )}
-                    {isClient(user.data) && (
+                    {isClient(user) && (
                         <Card.Text>
-                            <strong>Empresa:</strong> {user.data.company || 'N/A'}
+                            <strong>Empresa:</strong> {user.company || 'N/A'}
                         </Card.Text>
                     )}
                     <Card.Text>
-                        <strong>E-mail:</strong> {user.data.email}
+                        <strong>E-mail:</strong> {user.email}
                     </Card.Text>
                     <Button variant="primary" onClick={() => window.history.back()}>
                         Voltar
