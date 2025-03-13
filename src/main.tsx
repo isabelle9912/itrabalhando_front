2a396646d9649f858c4385ff7c30886c39a4dc6b
saahirah <isabellesaahirah28@gmail.com>
2025-03-13 15:43:12 -0300

    fix: remove providers from main

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {UserProvider} from "./context/UserContext.tsx";
-import {ProjectProvider} from "./context/ProjectContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <UserProvider>
-          <ProjectProvider>
-            <App />
-          </ProjectProvider>
-      </UserProvider>
-  </StrictMode>,
+    <App />
+  </StrictMode>
 )
