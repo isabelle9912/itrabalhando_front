992462cc47bec2104d06949e4b8194b45183127e
saahirah <isabellesaahirah28@gmail.com>
2025-03-13 15:43:58 -0300

    feat: adds conditional rendering and theme change button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,17 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { ThemeToggleButton } from "../theme/ThemeToggleButton.tsx";
+import { useUserContext } from "../../context/UserContext.tsx";
 
 const CustomNavbar = () => {
+    const navigate = useNavigate();
+    const { user, userLogout } = useUserContext();
+
+    const handleLogout = () => {
+        userLogout(); // Chama a função de logout do contexto
+        navigate('/login'); // Navega para a página de login
+    };
+
     return (
         <Navbar expand="lg">
             <Container>
@@ -11,6 +21,15 @@ const CustomNavbar = () => {
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/freelancers">Freelancers</Nav.Link>
                         <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                        {user ? ( // Verifica se o usuário está logado
+                            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                        ) : (
+                            <>
+                                <Nav.Link as={Link} to="/register">Registrar</Nav.Link>
+                                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            </>
+                        )}
+                        <Nav.Link><ThemeToggleButton/></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
